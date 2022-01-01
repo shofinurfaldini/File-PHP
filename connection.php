3fb9268ec3469087f072e1612c6f58f4e6406a29
Shofi Isnaeni Nurfaldini <shofinurfaldini@gmail.com>
2022-01-01 12:33:31 +0700

    Initial commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pwd = '';
+$db = 'db_perpus';
+
+$conn = mysqli_connect ($host, $user, $pwd, $db);
+
+?>
---
+++ create.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once ('connection.php');
+
+$id_buku = $_POST['id_buku'];
+$judul_buku = $_POST['judul_buku'];
+$nama_pengarang = $_POST['nama_pengarang'];
+$tahun_terbit = $_POST['tahun_terbit'];
+$penerbit = $_POST['penerbit'];
+
+if (!$id_buku || !$judul_buku || !$nama_pengarang || !$tahun_terbit || !$penerbit) {
+    echo json_encode (array ('message'=>'required field is empty.'));
+} else {
+    $query = mysqli_query ($conn, "INSERT INTO tb_student VALUES ('$id_buku', '$judul_buku', '$nama_pengarang', '$tahun_terbit', '$penerbit')");
+    
+    if ($query) {
+        echo json_encode (array ('message'=>'Data buku berhasil ditambahkan.'));
+    } else {
+        echo json_encode (array ('message'=>'Data buku gagal ditambahkan.'));
+    }
+}
+
+?>
